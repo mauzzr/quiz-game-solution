f21dd73d63c49a20d652d6086d2b7d609bac6a26
Peter Welby <pwelby.mauz@gmail.com>
2019-10-21 17:04:23 -0400

    Render stored scores in scores.html

---
+++ assets/js/scores.js
@@ -0,0 +1,30 @@
+/**
+ * File: scores.js
+ * Author: Peter Welby
+ * For use on the scores.html page, this script retrieves any saved scores from localStorage
+ * and renders them onto the page in a table.
+ */
+
+var tbodyElement = document.getElementById('scoresTableBody');
+var scores = JSON.parse(window.localStorage.getItem('quizScores'));
+// Loop vars
+var i, tableRowElement, tableHeadElement, initialsElement, scoreElement;
+
+for (i = 0; i < scores.length; i++) {
+    tableRowElement = document.createElement('tr');
+
+    tableHeadElement = document.createElement('thead');
+    tableHeadElement.setAttribute('scope', 'row');
+    tableHeadElement.textContent = `${i + 1}`;
+    tableRowElement.appendChild(tableHeadElement);
+
+    initialsElement = document.createElement('td');
+    initialsElement.textContent = scores[i].name;
+    tableRowElement.appendChild(initialsElement);
+
+    scoreElement = document.createElement('td');
+    scoreElement.textContent = scores[i].score;
+    tableRowElement.appendChild(scoreElement);
+
+    tbodyElement.appendChild(tableRowElement);
+}
